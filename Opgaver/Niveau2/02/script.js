78c0bcf243fb208883e18e7b3bae8972795381e5
Katrinerosa <katrinerosa@gmail.com>
2025-01-31 13:56:13 +0100

    niveau2opg2

---
+++ Opgaver/Niveau2/02/script.js
@@ -0,0 +1,3 @@
+let increaseBtn = document.querySelector(".card__increase")
+let decreaseBtn = document.querySelector(".card__decrease")
+let count = Number(document.querySelector("card__item-count").textContent)
